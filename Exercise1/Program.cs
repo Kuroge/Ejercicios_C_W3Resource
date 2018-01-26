222af27eec8664f7ef59c5a379a1e1b5f320e7c8
Adrián García Riera <i7610@softgal.com>
2018-01-26 14:14:17 +0100

    26/01/2018

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Adrián García Riera");
+            Console.ReadKey();
+        }
+    }
+}
