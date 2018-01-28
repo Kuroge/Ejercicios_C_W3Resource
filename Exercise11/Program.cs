a72c9d90879ceb85a8fc8f35cbc70a721de0f40c
Adrian García Riera <chgarciariera@gmail.com>
2018-01-28 18:40:13 +0100

    Ejercicios hechos con bucles

---
+++ Exercise11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor introduce tu edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Pareces mayor de {edad}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 4; i++)
+            {
+                if (i%2 == 0)
+                {
+                    for (int p = 0; p < 4; p++)
+                    {
+                        Console.Write("25 ");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int p = 0; p < 4; p++)
+                    {
+                        Console.Write("25");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
