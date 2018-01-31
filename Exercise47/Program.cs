19894e456137f1a86e65861357f82c1202a57259
Adrián García Riera <i7610@softgal.com>
2018-01-31 17:58:12 +0100

    last commit 31/01/18

---
+++ Exercise47/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[]{1,2,2,3,3,4,5,6,5,7,7,7,8,8,1};
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine($"Suma total: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
