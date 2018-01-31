516d00cd32dbf244c58b618cda5672541daf59d3
Adrián García Riera <i7610@softgal.com>
2018-01-31 12:18:46 +0100

    31/01/18

---
+++ Exercise34/ClassWord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise34
+{
+    class ClassWord
+    {
+        public string[] Word { get; private set; }
+
+        public ClassWord(string word)
+        {
+            Word = word.Split(' ');
+        }
+
+        public void ComprobarPalabra(string palabra)
+        {
+            if (Word[0].ToLower().Equals(palabra))
+            {
+                Console.WriteLine("La frase empieza por la palabra indicada");
+            }
+            else
+            {
+                Console.WriteLine("La frase no empieza por la palabra indicada");
+            }
+        }
+    }
+}
---
+++ Exercise34/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rep;
+            do
+            {
+                rep = false;
+                Console.WriteLine("Introduce una frase");
+                string input = Console.ReadLine();
+                ClassWord frase = new ClassWord(input);
+                Console.WriteLine("Introduce la palabra a comprobar");
+                string palabra = Console.ReadLine();
+                frase.ComprobarPalabra(palabra);
+                Console.WriteLine("¿Quieres comprobar otra frase? y/n");
+                char answer = Convert.ToChar(Console.ReadLine());
+                if (answer.Equals('y'))
+                {
+                    rep = true;
+                }
+            } while (rep);
+
+        }
+    }
+}
