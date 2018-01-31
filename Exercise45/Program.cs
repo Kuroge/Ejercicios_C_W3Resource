4d36ad37b6f34cddc9c29ee2edea98dd69a19567
Adrián García Riera <i7610@softgal.com>
2018-01-31 17:11:39 +0100

    31/01/18

---
+++ Exercise45/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[] { 1,3,4,5,6,7,8,4,3,2,5,4,2,6,7,8,9,5,4,3,2,1,5,6,7,6,4,5,3,2,1};
+            int ctr = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == input)
+                {
+                    ctr++;
+                }
+            }
+            Console.WriteLine($"El número {input} aparece {ctr} veces");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise46/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 4, 2, 3, 4, 5, 5 };
+            Console.WriteLine("Introduce un número");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((array[0] == num || array[array.Length - 1] == num));
+            Console.ReadKey();
+        }
+    }
+}
