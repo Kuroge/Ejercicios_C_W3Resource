b6e0c6e5777c7d9b803381350168389a1443974f
Adrián García Riera <i7610@softgal.com>
2018-01-31 13:52:05 +0100

    31/01/18

---
+++ Exercise40/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Console.WriteLine("Introduce el primer número");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo número");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int res1 = Math.Abs(num1 - x);
+            int res2 = Math.Abs(num2 - x);
+
+
+            Console.WriteLine(num1 == num2 ? 0 : (num1 > num2 ? num1 : num2));
+            Console.ReadKey();
+        }
+    }
+}
